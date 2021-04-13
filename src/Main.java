2f8d206abf4b1257619d5b5e998dd37165b1051b
duyettwyler2k <duyettwyller@gmail.com>
2021-04-13 13:56:29 +0700

    'dsa'

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Cylinder.getVolume(3,2));
+        System.out.println(Cylinder.getPerimeter(2));
+        System.out.println(Cylinder.getBaseArea(4));
+    }
+}
